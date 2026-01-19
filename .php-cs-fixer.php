0379d14f18e6d85ec5f0792025e0630c6c24d145
Jon Cram <webignition@gmail.com>
2026-01-19 18:24:02 +0000

    Create project skeleton

---
+++ .php-cs-fixer.php
@@ -0,0 +1,16 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__ . '/src')
+    ->in(__DIR__ . '/tests');
+
+$config = new PhpCsFixer\Config();
+return $config->setRules([
+    '@PER-CS' => true,
+    '@PhpCsFixer' => true,
+    'concat_space' => [
+        'spacing' => 'one',
+    ],
+    'php_unit_internal_class' => false,
+    'php_unit_test_class_requires_covers' => false,
+])->setFinder($finder);
